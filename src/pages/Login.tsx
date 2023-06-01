fd9c03a6947bd2d81fb61070d5240a96286057c5
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-01 16:09:59 +0100

    redirect the user to /home if he's already logged in

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Logo from '../assets/logo.png'
+import jwtDecode from 'jwt-decode'
 import { LoginForm } from '../components/LoginForm'
 import { SinupForm } from '../components/SinupForm'
+import { useNavigate } from 'react-router-dom'
+
 export const Login = () => {
   const [form, setForm] = useState('login')
+  const navigate = useNavigate()
+  const token = sessionStorage.getItem('AccessToken')
+  const isAuthenticated = token && !isTokenExpired(token)
+  interface Token {
+    exp: number
+  }
+  function isTokenExpired(token: string) {
+    const decodedToken = jwtDecode<Token>(token)
+    const currentTime = Date.now() / 1000
+    return decodedToken.exp < currentTime
+  }
+  useEffect(() => {
+    if (isAuthenticated) {
+      return navigate('/home')
+    }
+  }, [isAuthenticated, navigate])
+
   return (
     <div className="flex justify-items-center flex-wrap">
       <div className="m-auto mt-10">
