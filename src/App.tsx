d8968cddd1dd4292dd0a8663551243611d8daa37
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-01 13:49:33 +0100

    made the Authorization header automatique and made the tokens refresh on 401 status error

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,46 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { Login } from './pages/Login'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  axios.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem('AccessToken')
+    config.headers['Authorization'] = `Bearer ${token}`
+    return config
+  })
+
+  axios.interceptors.response.use(
+    (res) => {
+      return res
+    },
+    async (err) => {
+      if (err.response && err.response.status === 401) {
+        try {
+          const oldrefreshToken = sessionStorage.getItem('RefreshToken')
+          const response = await axios.post(
+            'http://localhost:5000/api/Authentication/refresh',
+            {
+              token: oldrefreshToken,
+            }
+          )
+
+          const { refreshToken, accessToken } = response.data.data
+          sessionStorage.setItem('AccessToken', accessToken)
+          sessionStorage.setItem('RefreshToken', refreshToken)
+
+          err.config.headers['Authorization'] = 'Bearer ' + accessToken
+          return axios(err.config)
+        } catch (error) {
+          console.error('Error refreshing token:', error)
+        }
+      }
+
+      return Promise.reject(err)
+    }
+  )
+
   return (
     <>
       <ToastContainer
