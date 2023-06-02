752aa6d3ac7ca5bcdfae9dfa22bbf72ed8194ad1
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-02 19:50:42 +0100

    created a context to handle the profile component animation

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,17 +1,30 @@
+import { createContext, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { PrivateRoutesWrapper } from './PrivateRoutesWrapper'
 import { Home } from '../pages/Home'
 import { Login } from '../pages/Login'
 
+interface HandleProfileClickContextType {
+  setAnimate: React.Dispatch<React.SetStateAction<string>>
+  animate: string
+}
+
+export const HandleProfileClickContext =
+  createContext<HandleProfileClickContextType | null>(null)
+
 export const AppRoutes = () => {
+  const [animate, setAnimate] = useState('close')
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index path="/" element={<Login />} />
-        <Route element={<PrivateRoutesWrapper />}>
-          <Route path="/home" element={<Home />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HandleProfileClickContext.Provider value={{ setAnimate, animate }}>
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<Login />} />
+          <Route element={<PrivateRoutesWrapper />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HandleProfileClickContext.Provider>
   )
 }
