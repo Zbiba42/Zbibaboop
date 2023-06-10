c11b50c7b0059a9de3e693f30838fc9367649ca2
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-10 18:25:36 +0100

    created the upload midleware for multiple files

--- back-end/Routes/user.js
+++ back-end/Routes/user.js
@@ -1,6 +1,29 @@
 const router = require('express').Router()
-const { getUser } = require('../Controllers/userController.js')
+const { getUser, updateUser } = require('../Controllers/userController.js')
+const multer = require('multer')
+const fs = require('fs')
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const userId = req.body.id
+    const uploadPath = `./uploads/${userId}`
+    fs.mkdirSync(uploadPath, { recursive: true })
+    callback(null, uploadPath)
+  },
+  filename: (req, file, callback) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+    const extension = file.originalname.split('.').pop()
+    callback(null, file.fieldname + '-' + uniqueSuffix + '.' + extension)
+  },
+})
+
+const upload = multer({ storage })
+const uploadMiddleware = upload.fields([
+  { name: 'ProfilePath' },
+  { name: 'CoverPath' },
+])
 
 router.get('/getUser', getUser)
+
+router.post('/updateUser', uploadMiddleware, updateUser)
 
 module.exports = router
