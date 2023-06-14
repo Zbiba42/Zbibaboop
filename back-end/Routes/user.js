61d34365094bec9727e2c2d60f7b93928a3e7903
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-15 00:58:23 +0100

    called middleware and created route for search

--- back-end/Routes/user.js
+++ back-end/Routes/user.js
@@ -1,26 +1,12 @@
 const router = require('express').Router()
-const { getUser, updateUser } = require('../Controllers/userController.js')
-const multer = require('multer')
-const fs = require('fs')
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    const userId = req.body.id
-    const uploadPath = `./uploads/${userId}`
-    fs.mkdirSync(uploadPath, { recursive: true })
-    callback(null, uploadPath)
-  },
-  filename: (req, file, callback) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
-    const extension = file.originalname.split('.').pop()
-    callback(null, file.fieldname + '-' + uniqueSuffix + '.' + extension)
-  },
-})
+const { uploadMiddleware } = require('../middlewares/multerImages.js')
+const {
+  search,
+  getUser,
+  updateUser,
+} = require('../Controllers/userController.js')
 
-const upload = multer({ storage })
-const uploadMiddleware = upload.fields([
-  { name: 'ProfilePath' },
-  { name: 'CoverPath' },
-])
+router.get('/search', search)
 
 router.get('/getUser', getUser)
 
