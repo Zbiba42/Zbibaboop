93317c734f591db14d24a12cf63426064b6ad5e4
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-15 14:17:49 +0100

    updated the getUser function and made the search return pages of 10

--- back-end/Controllers/userController.js
+++ back-end/Controllers/userController.js
@@ -3,22 +3,44 @@ const User = require('../models/user')
 const search = async (req, res) => {
   try {
     const query = req.query.search
-    const users = await User.find({
+    const page = parseInt(req.query.page) || 1 // Current page number (default: 1)
+    const limit = parseInt(req.query.limit) || 10 // Results per page (default: 10)
+    const skip = (page - 1) * limit // Number of results to skip
+    const users = await User.find(
+      {
+        Fullname: { $regex: query, $options: 'i' },
+      },
+      { Verified: 0, Password: 0, friendRequests: 0 }
+    )
+      .skip(skip)
+      .limit(limit)
+    const totalUsers = await User.countDocuments({
       Fullname: { $regex: query, $options: 'i' },
     })
-    res.status(200).json({ succes: true, data: users })
+
+    const totalPages = Math.ceil(totalUsers / limit)
+    res.status(200).json({
+      success: true,
+      data: users,
+      currentPage: page,
+      totalPages: totalPages,
+    })
   } catch (error) {
     res.status(404).json({ succes: false, error: error.message })
   }
 }
 
 const getUser = async (req, res) => {
-  try {
-    const id = req.query.id
-    const user = await User.findOne({ _id: id })
-    res.status(200).json({ succes: true, data: user })
-  } catch (error) {
-    res.status(400).json({ succes: false, error: error.message })
+  if (req.payload.id == req.query.id) {
+    try {
+      const id = req.query.id
+      const user = await User.findOne({ _id: id })
+      res.status(200).json({ succes: true, data: user })
+    } catch (error) {
+      res.status(400).json({ succes: false, error: error.message })
+    }
+  } else {
+    res.status(400).json({ succes: false, error: 'YOU ARE NOT AUTHORIZED !' })
   }
 }
 
