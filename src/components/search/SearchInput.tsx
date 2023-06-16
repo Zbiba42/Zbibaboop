c8da3448d90e8d8c483a691b03aa36cd84418b7d
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-16 21:40:57 +0100

    made the search input a single component

---
+++ src/components/search/SearchInput.tsx
@@ -0,0 +1,19 @@
+import { TextField } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+
+export const SearchInput = () => {
+  const navigate = useNavigate()
+  return (
+    <TextField
+      placeholder="Search"
+      variant="outlined"
+      sx={{ width: '40%', position: 'fixed', left: '25rem', top: '1.8rem' }}
+      onKeyDown={(e: any) => {
+        e.key == 'Enter' && e.target.value.trim() != ''
+          ? navigate('/search')
+          : ''
+      }}
+      autoComplete="off"
+    />
+  )
+}
