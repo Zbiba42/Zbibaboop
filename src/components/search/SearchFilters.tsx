9ce0127abae3757d54c2413e7390b05717f96aff
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-16 21:42:07 +0100

    created the search filters component

---
+++ src/components/search/SearchFilters.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { Typography } from '@mui/material'
+import FeedIcon from '@mui/icons-material/Feed'
+export const SearchFilters = () => {
+  const [selected, setSelected] = useState<string>('All')
+  const filters = [
+    { name: 'All', icon: 'fa-regular fa-newspaper' },
+    { name: 'People', icon: 'fa-solid fa-user-group' },
+    { name: 'Posts', icon: 'fa-regular fa-newspaper' },
+    { name: 'placeHolder', icon: 'fa-regular fa-newspaper' },
+  ]
+  return (
+    <div className="fixed top-0 h-[100vh] text-start w-64 bg-[#f9f9f9] pt-2 border-r">
+      <Typography
+        component={'h1'}
+        sx={{
+          fontWeight: 700,
+          fontSize: '1.2em',
+          borderBottom: 1,
+          padding: 1,
+        }}
+      >
+        Search Results
+      </Typography>
+      <Typography
+        component={'h1'}
+        sx={{
+          fontWeight: 700,
+          fontSize: '1em',
+          margin: 2,
+        }}
+      >
+        Filters
+      </Typography>
+      <div className="w-[100%] h-[100%] p-1">
+        {filters.map((link) => {
+          return (
+            <div
+              className={
+                selected == link.name.toString()
+                  ? 'bg-[#e5e5dc] w-[100%] h-10 border flex items-center mb-1 rounded-md p-2 hover:bg-[#e5e5dc] cursor-pointer'
+                  : 'w-[100%] h-10 border flex items-center mb-1 rounded-md p-2 hover:bg-[#e5e5dc] cursor-pointer'
+              }
+              onClick={() => setSelected(link.name)}
+            >
+              <div className="bg-[#e5e5dc] rounded-full w-[30px] h-[30px] flex justify-center items-center">
+                {link.name == 'Posts' ? (
+                  <FeedIcon fontSize="small" />
+                ) : (
+                  <i className={link.icon + ' w-4 h-4'}></i>
+                )}
+              </div>
+              <Typography
+                component={'h3'}
+                sx={{
+                  fontWeight: 500,
+                  fontSize: '1em',
+                  margin: 2,
+                  textTransform: 'capitalize',
+                }}
+              >
+                {link.name}
+              </Typography>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
