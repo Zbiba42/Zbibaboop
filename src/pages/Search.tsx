fad2c49f131548080dd13d2399b65513a4007736
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-17 15:59:47 +0100

    finished working on the search function

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from 'react'
 import { SearchFilters } from '../components/search/SearchFilters'
-
+import { useLocation } from 'react-router-dom'
+import TextField from '@mui/material/TextField/TextField'
+import axios from 'axios'
+import { serverUrl } from '../config'
 export const Search = () => {
+  const location = useLocation()
+  const [filter, setFilter] = useState<string>('All')
+  const [results, setResults] = useState()
+  const handleSearch = async (filter: string, search: string) => {
+    try {
+      const { data } = await axios.get(
+        serverUrl + `/api/search/?search=${search}&page=1`
+      )
+      setResults(data.data)
+    } catch (error) {}
+  }
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search)
+    const query = searchParams.get('search') as string
+    const filter = searchParams.get('filter') as string
+    if (filter != '' && query != '') {
+      handleSearch(filter, query)
+    }
+  }, [])
   return (
     <>
       <div
@@ -12,7 +35,18 @@ export const Search = () => {
           paddingTop: '1.8rem',
         }}
       >
-        <SearchFilters />
+        <TextField
+          placeholder="Search"
+          variant="outlined"
+          sx={{ width: '40%', position: 'fixed', left: '25rem', top: '1.8rem' }}
+          onKeyDown={(e: any) => {
+            e.key == 'Enter' && e.target.value.trim() != ''
+              ? handleSearch(filter, e.target.value.trim())
+              : ''
+          }}
+          autoComplete="off"
+        />
+        <SearchFilters filter={filter} setFilter={setFilter} />
       </div>
     </>
   )
