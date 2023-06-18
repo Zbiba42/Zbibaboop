f3b4a1eb74a6e6bd58adb5584544ca95b56a82ff
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-18 17:02:40 +0100

    added link to user account

--- src/components/Profilemin.tsx
+++ src/components/Profilemin.tsx
@@ -1,57 +1,48 @@
+import { Link } from 'react-router-dom'
 import { serverUrl } from '../config'
 import { User } from '../pages/Search'
-import { Button, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
 
 interface Props {
   user: User
 }
 export const Profilemin = ({ user }: Props) => {
+  console.log(user)
   return (
-    <div className="w-[100%] p-2 m-2 rounded-lg border flex flex-wrap relative -z-10">
-      <img
-        src={serverUrl + user.ProfilePath}
-        alt=""
-        className="w-16 h-16 rounded-full object-cover"
-      />
-      <div className="m-2 text-start">
-        <Typography
-          component={'h2'}
-          sx={{
-            fontWeight: 700,
-            fontSize: '1.2em',
-
-            color: '#252526',
-          }}
-        >
-          {user.Fullname}
-        </Typography>
-        {user.Country ? (
+    <Link to={'/user/' + user._id}>
+      <div className="w-[100%] p-2 m-2 rounded-lg border flex flex-wrap cursor-pointer hover:bg-gray-100">
+        <img
+          src={serverUrl + user.ProfilePath}
+          alt=""
+          className="w-16 h-16 rounded-full object-cover"
+        />
+        <div className="m-2 text-start">
           <Typography
-            component={'h4'}
+            component={'h2'}
             sx={{
-              fontWeight: 400,
-              color: 'grey',
+              fontWeight: 700,
+              fontSize: '1.2em',
+
+              color: '#252526',
             }}
           >
-            Lives In {user.City}, {user.Country}
+            {user.Fullname}
           </Typography>
-        ) : (
-          ''
-        )}
+          {user.Country ? (
+            <Typography
+              component={'h4'}
+              sx={{
+                fontWeight: 400,
+                color: 'grey',
+              }}
+            >
+              Lives In {user.City}, {user.Country}
+            </Typography>
+          ) : (
+            ''
+          )}
+        </div>
       </div>
-      <Button
-        variant="outlined"
-        sx={{
-          position: 'absolute',
-          right: '0.8rem',
-          top: '1.4rem',
-          height: '2.5rem',
-          color: '#272838',
-          border: '1px solid #272838',
-        }}
-      >
-        View Profile
-      </Button>
-    </div>
+    </Link>
   )
 }
