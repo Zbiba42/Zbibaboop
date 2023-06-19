cccb4008911f21ddd4eb0c39de0e15d20f7935af
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-19 20:54:44 +0100

    added route to check the relation between two users

--- back-end/Routes/user.js
+++ back-end/Routes/user.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
 const { uploadMiddleware } = require('../middlewares/multerImages.js')
-const { getUser, updateUser } = require('../Controllers/userController.js')
+const {
+  getUser,
+  updateUser,
+  checkUsersRelation,
+} = require('../Controllers/userController.js')
 
 router.get('/getUser', getUser)
+
+router.get('/getRelation', checkUsersRelation)
 
 router.post('/updateUser', uploadMiddleware, updateUser)
 
