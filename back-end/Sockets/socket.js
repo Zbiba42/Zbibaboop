4990e5d1a701c9342d77d251404e85da909d4438
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-24 19:33:02 +0100

    created socket for canceling friend request

--- back-end/Sockets/socket.js
+++ back-end/Sockets/socket.js
@@ -9,6 +9,8 @@ module.exports = (io) => {
       try {
         const friendRequest = await FriendReq.create(data)
         const notification = await Notification.create({
+          sender: data.sender,
+          receiver: data.Receiver,
           type: 'friend request',
           content: friendRequest,
         })
@@ -25,11 +27,46 @@ module.exports = (io) => {
             },
           }
         )
-        io.to(data.Receiver).emit('friend Request', friendRequest)
+        io.to(data.Receiver).emit('notification', notification)
         io.to(data.sender).emit('FriendReqSent', {
           succes: true,
           data: 'friend request was sent !',
         })
+      } catch (error) {}
+    })
+    socket.on('cancelFriendReq', async (data) => {
+      try {
+        const friendRequest = await FriendReq.findOne(data)
+        const notifquery = {
+          type: 'friend request',
+          sender: data.sender,
+          receiver: data.Receiver,
+        }
+        const notification = await Notification.findOne(notifquery)
+        await Notification.deleteOne(notifquery)
+        await FriendReq.deleteOne(data)
+        await User.updateOne(
+          { _id: data.sender },
+          {
+            $pull: {
+              friendRequestsSent: friendRequest.id,
+            },
+          }
+        )
+        await User.updateOne(
+          { _id: data.Receiver },
+          {
+            $pull: {
+              friendRequestsReceived: friendRequest.id,
+              notifications: notification.id,
+            },
+          }
+        )
+        io.to(data.sender).emit('FriendReqCanceled', {
+          succes: true,
+          data: 'friend request was canceled',
+        })
+        io.to(data.Receiver).emit('cancelNotif', notification)
       } catch (error) {}
     })
     socket.on('disconnect', () => {
