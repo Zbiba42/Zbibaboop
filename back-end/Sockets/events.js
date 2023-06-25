0e103fc04b3f7abedb1b31b45b5d4ff26c284989
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-25 15:18:56 +0100

    created event to accept friend request

--- back-end/Sockets/events.js
+++ back-end/Sockets/events.js
@@ -67,4 +67,71 @@ const cancelFriendReq = async (io, data) => {
     io.to(data.Receiver).emit('cancelNotif', notification)
   } catch (error) {}
 }
-module.exports = { sendFriendReq, cancelFriendReq }
+
+const acceptFriendReq = async (io, data) => {
+  try {
+    const friendRequest = await FriendReq.findOne(data.content)
+    const notifquery = {
+      type: 'friend request',
+      sender: data.sender,
+      receiver: data.receiver,
+    }
+    const notification = await Notification.findOne(notifquery)
+    // add friend for both sender and receiver
+    await User.updateOne(
+      { _id: data.receiver },
+      { $push: { friends: data.sender } }
+    )
+    await User.updateOne(
+      { _id: data.sender },
+      { $push: { friends: data.receiver } }
+    )
+    // remove the friend requsets for them both
+    await User.updateOne(
+      { _id: data.sender },
+      {
+        $pull: {
+          friendRequestsSent: friendRequest.id,
+        },
+      }
+    )
+    // remove the notification as well for the recevier
+    await User.updateOne(
+      { _id: data.receiver },
+      {
+        $pull: {
+          friendRequestsReceived: friendRequest.id,
+          notifications: notification.id,
+        },
+      }
+    )
+    // delete the notif and friend request in the end
+    await Notification.deleteOne(notifquery)
+    await FriendReq.deleteOne(data.content)
+    // create notif for the sender
+    const SenderNotification = await Notification.create({
+      sender: data.receiver,
+      receiver: data.sender,
+      type: 'friend request accepted',
+      content: '',
+    })
+    await User.updateOne(
+      { _id: data.sender },
+      { $push: { notifications: SenderNotification } }
+    )
+    // send notifs for both
+    io.to(data.sender).emit('friendReqAccepted', {
+      succes: true,
+      data: SenderNotification,
+    })
+    io.to(data.receiver).emit('friendReqAcceptedSuccess', {
+      succes: true,
+      data: 'you are now friends with ',
+      id: data.sender,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = { sendFriendReq, cancelFriendReq, acceptFriendReq }
