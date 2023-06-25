8d0126a3e3c178d0a49562e3e4eae811f59b2fc9
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-25 15:21:38 +0100

    created notif to see when someone accepts your friend request

--- src/components/navBar/Notification.tsx
+++ src/components/navBar/Notification.tsx
@@ -2,6 +2,9 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { serverUrl } from '../../config'
 import { profile } from '../Profile/ProfileContent'
+
+import { FriendRequest } from './friendRequest'
+
 interface Props {
   notif: {
     sender: String
@@ -14,8 +17,9 @@ interface Props {
     }
     status: String
   }
+  getNotifs: () => void
 }
-export const Notification = ({ notif }: Props) => {
+export const Notification = ({ notif, getNotifs }: Props) => {
   const [sender, setSender] = useState<profile>()
   const getSender = async () => {
     const { data } = await axios.get(serverUrl + '/api/user/getUser', {
@@ -28,8 +32,10 @@ export const Notification = ({ notif }: Props) => {
   useEffect(() => {
     getSender()
   }, [])
-  return (
-    <>
+  if (notif.type === 'friend request') {
+    return <FriendRequest notif={notif} sender={sender} getNotifs={getNotifs} />
+  } else if (notif.type === 'friend request accepted') {
+    return (
       <div className="w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto">
         <div className="p-2">
           <div className="flex items-start">
@@ -40,20 +46,14 @@ export const Notification = ({ notif }: Props) => {
                 alt=""
               />{' '}
               <p className="text-sm font-medium leading-5 text-gray-900 m-1">
-                {sender?.Fullname} sent you a friend request
+                {sender?.Fullname} Accepted your friend request
               </p>
-              <div className="w-[100%] flex justify-center space-x-2 mt-2">
-                <button className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300">
-                  Accept
-                </button>
-                <button className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300">
-                  Decline
-                </button>
-              </div>
             </div>
           </div>
         </div>
       </div>
-    </>
-  )
+    )
+  } else {
+    return <h2></h2>
+  }
 }
