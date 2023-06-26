5c0091110483b218696ba8a01f9f11fb068770e0
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-26 15:37:56 +0100

    created function to read notif and made the notif redirect to the sender profile

--- src/components/navBar/Notification.tsx
+++ src/components/navBar/Notification.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 import { serverUrl } from '../../config'
-import { profile } from '../Profile/ProfileContent'
-
-import { FriendRequest } from './friendRequest'
+import { FriendRequest } from './FriendRequest'
+import { profile } from '../../pages/User'
+import { Link } from 'react-router-dom'
 
 interface Props {
   notif: {
@@ -17,7 +17,7 @@ interface Props {
     }
     status: String
   }
-  getNotifs: () => void
+  getNotifs: () => Promise<void>
 }
 export const Notification = ({ notif, getNotifs }: Props) => {
   const [sender, setSender] = useState<profile>()
@@ -29,29 +29,57 @@ export const Notification = ({ notif, getNotifs }: Props) => {
     })
     setSender(data.data)
   }
+  const readNotif = async () => {
+    if (notif.status === 'unread') {
+      const { data } = await axios.post(serverUrl + '/api/user/readNotif', {
+        notif: notif,
+      })
+      if (data.succes) {
+        getNotifs()
+      }
+    } else {
+      return
+    }
+  }
   useEffect(() => {
     getSender()
   }, [])
+
   if (notif.type === 'friend request') {
-    return <FriendRequest notif={notif} sender={sender} getNotifs={getNotifs} />
+    return (
+      <FriendRequest
+        notif={notif}
+        sender={sender}
+        readNotif={readNotif}
+        getNotifs={getNotifs}
+      />
+    )
   } else if (notif.type === 'friend request accepted') {
     return (
-      <div className="w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto">
-        <div className="p-2">
-          <div className="flex items-start">
-            <div className="ml-3 w-0 flex-1 pt-0.5">
-              <img
-                className=" rounded-full  w-9 h-9 mr-2 object-cover float-left"
-                src={serverUrl + sender?.ProfilePath}
-                alt=""
-              />{' '}
-              <p className="text-sm font-medium leading-5 text-gray-900 m-1">
-                {sender?.Fullname} Accepted your friend request
-              </p>
+      <Link to={`/user/${sender?._id}`}>
+        <div
+          className=" relative w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto"
+          onClick={readNotif}
+        >
+          {notif.status === 'unread' && (
+            <div className="absolute top-2 right-2 w-2 h-2 bg-blue-500 rounded-full"></div>
+          )}
+          <div className="p-2">
+            <div className="flex items-start">
+              <div className="ml-3 w-0 flex-1 pt-0.5">
+                <img
+                  className=" rounded-full  w-9 h-9 mr-2 object-cover float-left"
+                  src={serverUrl + sender?.ProfilePath}
+                  alt=""
+                />{' '}
+                <p className="text-sm font-medium leading-5 text-gray-900 m-1">
+                  {sender?.Fullname} Accepted your friend request
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     )
   } else {
     return <h2></h2>
