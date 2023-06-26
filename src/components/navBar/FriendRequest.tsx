a0abe0530d7bdcbb191a04934368d5442cdd3aeb
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-26 16:11:18 +0100

    changed the component structre and created function to delete a notif

--- src/components/navBar/FriendRequest.tsx
+++ src/components/navBar/FriendRequest.tsx
@@ -20,12 +20,14 @@ interface Props {
   sender: profile | undefined
   getNotifs: () => void
   readNotif: () => Promise<void>
+  removeNotif: () => Promise<void>
 }
 export const FriendRequest = ({
   notif,
   sender,
   getNotifs,
   readNotif,
+  removeNotif,
 }: Props) => {
   const socket = useContext(SocketContext)
   const acceptFriend = () => {
@@ -51,43 +53,47 @@ export const FriendRequest = ({
     })
   }
   return (
-    <Link to={`/user/${sender?._id}`}>
-      <div
-        className="relative w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto cursor-pointer"
-        onClick={readNotif}
-      >
-        {notif.status === 'unread' && (
-          <div className="absolute top-2 right-2 w-2 h-2 bg-blue-500 rounded-full"></div>
-        )}
-        <div className="p-2">
-          <div className="flex items-start">
-            <div className="ml-3 w-0 flex-1 pt-0.5">
+    <div className="relative w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto ">
+      {notif.status === 'unread' && (
+        <div className="absolute top-2 right-2 w-2 h-2 bg-blue-500 rounded-full"></div>
+      )}
+      {notif.status === 'read' && (
+        <i
+          className="fa-solid fa-xmark absolute top-2 right-3 w-2 h-2 rounded-full cursor-pointer"
+          onClick={removeNotif}
+        ></i>
+      )}
+      <div className="p-2">
+        <div className="flex items-start">
+          <div className="ml-3 w-0 flex-1 pt-0.5">
+            <Link to={`/user/${sender?._id}`}>
               <img
                 className=" rounded-full  w-9 h-9 mr-2 object-cover float-left"
                 src={serverUrl + sender?.ProfilePath}
                 alt=""
+                onClick={readNotif}
               />{' '}
-              <p className="text-sm font-medium leading-5 text-gray-900 m-1">
-                {sender?.Fullname} sent you a friend request
-              </p>
-              <div className="w-[100%] flex justify-center space-x-2 mt-2">
-                <button
-                  className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
-                  onClick={acceptFriend}
-                >
-                  Accept
-                </button>
-                <button
-                  className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
-                  onClick={declineFriend}
-                >
-                  Decline
-                </button>
-              </div>
+            </Link>
+            <p className="text-sm font-medium leading-5 text-gray-900 m-1">
+              {sender?.Fullname} sent you a friend request
+            </p>
+            <div className="w-[100%] flex justify-center space-x-2 mt-2">
+              <button
+                className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
+                onClick={acceptFriend}
+              >
+                Accept
+              </button>
+              <button
+                className="px-3 py-1 text-gray-800 bg-gray-200 rounded hover:bg-gray-300 focus:outline-none focus:bg-gray-300"
+                onClick={declineFriend}
+              >
+                Decline
+              </button>
             </div>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
--- src/components/navBar/Notification.tsx
+++ src/components/navBar/Notification.tsx
@@ -41,6 +41,14 @@ export const Notification = ({ notif, getNotifs }: Props) => {
       return
     }
   }
+  const removeNotif = async () => {
+    const { data } = await axios.post(serverUrl + '/api/user/removeNotif', {
+      notif: notif,
+    })
+    if (data.succes) {
+      getNotifs()
+    }
+  }
   useEffect(() => {
     getSender()
   }, [])
@@ -52,34 +60,39 @@ export const Notification = ({ notif, getNotifs }: Props) => {
         sender={sender}
         readNotif={readNotif}
         getNotifs={getNotifs}
+        removeNotif={removeNotif}
       />
     )
   } else if (notif.type === 'friend request accepted') {
     return (
-      <Link to={`/user/${sender?._id}`}>
-        <div
-          className=" relative w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto"
-          onClick={readNotif}
-        >
-          {notif.status === 'unread' && (
-            <div className="absolute top-2 right-2 w-2 h-2 bg-blue-500 rounded-full"></div>
-          )}
-          <div className="p-2">
-            <div className="flex items-start">
-              <div className="ml-3 w-0 flex-1 pt-0.5">
+      <div className=" relative w-full mb-3 bg-white shadow-sm outline outline-1 outline-gray-200  rounded-lg pointer-events-auto">
+        {notif.status === 'unread' && (
+          <div className="absolute top-2 right-2 w-2 h-2 bg-blue-500 rounded-full"></div>
+        )}
+        {notif.status === 'read' && (
+          <i
+            className="fa-solid fa-xmark absolute top-2 right-3 w-2 h-2 rounded-full cursor-pointer"
+            onClick={removeNotif}
+          ></i>
+        )}
+        <div className="p-2">
+          <div className="flex items-start">
+            <div className="ml-3 w-0 flex-1 pt-0.5">
+              <Link to={`/user/${sender?._id}`}>
                 <img
                   className=" rounded-full  w-9 h-9 mr-2 object-cover float-left"
                   src={serverUrl + sender?.ProfilePath}
                   alt=""
+                  onClick={readNotif}
                 />{' '}
-                <p className="text-sm font-medium leading-5 text-gray-900 m-1">
-                  {sender?.Fullname} Accepted your friend request
-                </p>
-              </div>
+              </Link>
+              <p className="text-sm font-medium leading-5 text-gray-900 m-1">
+                {sender?.Fullname} Accepted your friend request
+              </p>
             </div>
           </div>
         </div>
-      </Link>
+      </div>
     )
   } else {
     return <h2></h2>
