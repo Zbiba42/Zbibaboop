1b561773a35dcb82066a63b0ad89f34769c00ac7
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-27 20:20:12 +0100

    finished the friends Tab

--- src/components/Profile/tabs/Friends.tsx
+++ src/components/Profile/tabs/Friends.tsx
@@ -1,3 +1,62 @@
-export const Friends = () => {
-  return <div>Friends</div>
+import { useState, useEffect } from 'react'
+import { Profilemin } from '../../Profilemin'
+import { Box } from '@mui/material'
+import { Pagination } from '@mui/material'
+interface Props {
+  friends?: Array<{
+    _id: string
+    Fullname: string
+    ProfilePath: string
+    Country: string
+    City: string
+  }>
+}
+export const Friends = ({ friends }: Props) => {
+  const [pageNumber, setPageNumber] = useState(1)
+  const [friendsOnPage, setFriendsOnPage] = useState<Props['friends']>([])
+  const [totalPages, setTotalPages] = useState(0)
+  const pagination = (_event: React.ChangeEvent<unknown>, value: number) => {
+    setPageNumber(value)
+  }
+  useEffect(() => {
+    if (friends) {
+      // Calculate the total number of pages
+      setTotalPages(Math.ceil(friends?.length / 10))
+
+      // Calculate the starting index and ending index for the friends on the current page
+      const startIndex = (pageNumber - 1) * 10
+      const endIndex = Math.min(startIndex + 10, friends.length)
+
+      // Retrieve the friends to be displayed on the current page
+      setFriendsOnPage(friends.slice(startIndex, endIndex))
+    }
+  }, [pageNumber])
+  return (
+    <>
+      <Box
+        sx={{
+          padding: 0,
+          textAlign: 'left',
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          flexWrap: 'wrap',
+          gap: '3%',
+        }}
+      >
+        {friendsOnPage?.map((friend) => {
+          return (
+            <div style={{ flexBasis: '48%' }}>
+              <Profilemin user={friend} />
+            </div>
+          )
+        })}
+        <Pagination
+          page={pageNumber}
+          count={totalPages}
+          onChange={pagination}
+        />
+      </Box>
+    </>
+  )
 }
