95cfcc2681303120c08ee19886c181500c87cec3
Zbiba <abdrrahmanabdo46@gmail.com>
2023-06-30 07:02:53 +0100

    wrapped everything with the provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 )
