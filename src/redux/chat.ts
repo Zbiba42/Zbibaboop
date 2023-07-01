1f96a966089b3c8831b3be7fae055c77614c4459
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-01 20:00:30 +0100

    created slice for chats on screen

--- src/redux/chat.ts
+++ src/redux/chat.ts
@@ -1,13 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+interface StateInterface {
+  newChatShown: boolean
+  Chats: Array<HTMLDivElement>
+}
 const ChatSlice = createSlice({
   name: 'Chat',
   initialState: {
     newChatShown: false,
-  },
+    Chats: [],
+  } as StateInterface,
   reducers: {
     toggleNewChatComponent: (state) => {
       state.newChatShown = !state.newChatShown
+    },
+    AddChat: (state, action) => {
+      if (state.Chats.length <= 3) {
+        state.Chats = [...state.Chats, action.payload.chat]
+      } else {
+        state.Chats = [...state.Chats.slice(1), action.payload.chat]
+      }
     },
   },
 })
