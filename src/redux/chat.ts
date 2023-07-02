a977cfead8216ab8ceac38b08cdfd7fbf27c415f
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-02 20:13:24 +0100

    fixed the add chat function and created function to remove chat in the store redux

--- src/redux/chat.ts
+++ src/redux/chat.ts
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { profile } from '../components/Profile/ProfileContent'
 
 interface StateInterface {
   newChatShown: boolean
-  Chats: Array<HTMLDivElement>
+  Chats: Array<{ recipient: profile }>
 }
 const ChatSlice = createSlice({
   name: 'Chat',
@@ -15,15 +16,31 @@ const ChatSlice = createSlice({
       state.newChatShown = !state.newChatShown
     },
     AddChat: (state, action) => {
-      if (state.Chats.length <= 3) {
-        state.Chats = [...state.Chats, action.payload.chat]
-      } else {
-        state.Chats = [...state.Chats.slice(1), action.payload.chat]
+      const recipient = action.payload.recipient
+      const isRecipientExists = state.Chats.some(
+        (chat) => chat.recipient._id === recipient._id
+      )
+
+      if (!isRecipientExists) {
+        if (state.Chats.length < 3) {
+          state.Chats = [...state.Chats, { recipient }]
+        } else {
+          state.Chats = [...state.Chats.slice(1), { recipient }]
+        }
+      }
+    },
+    removeChat: (state, action) => {
+      const chatIndex = state.Chats.findIndex(
+        (chat) => chat.recipient._id === action.payload
+      )
+
+      if (chatIndex !== -1) {
+        state.Chats.splice(chatIndex, 1)
       }
     },
   },
 })
 
-export const { toggleNewChatComponent } = ChatSlice.actions
+export const { toggleNewChatComponent, AddChat, removeChat } = ChatSlice.actions
 
 export default ChatSlice.reducer
