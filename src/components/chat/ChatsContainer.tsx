c61b4656591096eb03a3dc1c24af8e25c587b322
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-02 20:14:33 +0100

    rename , creating a chat Component on each chat on the redux store

--- src/components/chat/ChatsContainer.tsx
+++ src/components/chat/ChatsContainer.tsx
@@ -3,16 +3,20 @@ import { Box } from '@mui/material'
 import { useSelector } from 'react-redux'
 import { PickMsgReceiver } from './PickMsgReceiver'
 import { NewChat } from './NewChat'
+import { Chat } from './Chat'
+import { profile } from '../Profile/ProfileContent'
 
 export const ChatContainer = () => {
   const newChatShown = useSelector((state: any) => state.Chat.newChatShown)
+  const Chats = useSelector((state: any) => state.Chat.Chats)
+  console.log(Chats)
   return (
     <Box
       sx={{
-        width: '95%',
         height: '65%',
         paddingRight: '100px',
         position: 'absolute',
+        zIndex: 99,
         bottom: 0,
         right: 0,
         display: 'flex',
@@ -20,6 +24,9 @@ export const ChatContainer = () => {
       }}
     >
       {newChatShown && <PickMsgReceiver />}
+      {Chats.map((chat: { recipient: profile }) => {
+        return <Chat recipient={chat.recipient} />
+      })}
       <NewChat />
     </Box>
   )
