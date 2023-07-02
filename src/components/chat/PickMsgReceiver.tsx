fe09120524df95df033c764e0980b1f0af6f2956
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-02 20:15:06 +0100

    small changes and added a new results components on the results

--- src/components/chat/PickMsgReceiver.tsx
+++ src/components/chat/PickMsgReceiver.tsx
@@ -2,10 +2,16 @@ import { Box, TextField } from '@mui/material'
 import axios from 'axios'
 import { ChangeEvent, useState } from 'react'
 import { serverUrl } from '../../config'
-import { Profilemin } from '../Profilemin'
+import { useDispatch } from 'react-redux'
+import { profile } from '../Profile/ProfileContent'
+import { toggleNewChatComponent } from '../../redux/chat'
+import CloseIcon from '@mui/icons-material/Close'
+import { FriendsResults } from './FriendsResults'
 
 export const PickMsgReceiver = () => {
   const [Results, setResults] = useState([])
+  const dispatch = useDispatch()
+
   const handleSearchChange = async (event: ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value
     if (searchTerm.trim().length > 0) {
@@ -20,23 +26,32 @@ export const PickMsgReceiver = () => {
   return (
     <Box
       sx={{
+        backgroundColor: 'white',
         width: 320,
         marginLeft: '20px',
         border: '1px solid grey',
         boxShadow: 2,
-        borderRadius: 2,
+        borderRadius: '10px 10px 0% 0%',
         pt: 2,
         textAlign: 'start',
         display: 'flex',
         flexDirection: 'column',
+        position: 'relative',
       }}
     >
+      <button
+        className="absolute right-2 top-2"
+        onClick={() => dispatch(toggleNewChatComponent())}
+      >
+        <CloseIcon fontSize="small" sx={{ color: 'black' }} />
+      </button>
+
       <h3 className="text-lg font-medium ml-4">New Message</h3>
+
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'row',
-          mb: 2,
         }}
       >
         <h3 className="text-base font-medium m-2 ml-6">To :</h3>
@@ -50,9 +65,11 @@ export const PickMsgReceiver = () => {
       </Box>
 
       <hr className="w-full" />
-      {Results.map((friend) => {
-        return <Profilemin user={friend} />
-      })}
+      <Box sx={{ overflow: 'scroll', height: '100%' }}>
+        {Results.map((friend: profile) => {
+          return <FriendsResults friend={friend} />
+        })}
+      </Box>
     </Box>
   )
 }
