22095ae5983afa01fc11a59d2b5100cce5f73682
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-10 21:25:13 +0100

    created function to get messages with pagination

---
+++ back-end/Controllers/messagesController.js
@@ -0,0 +1,24 @@
+const Conversation = require('../models/conversation')
+
+const getMessages = async (req, res) => {
+  const participants = [req.payload.id, req.query.recipient]
+  const pageSize = 40
+  const pageNumber = req.query.page
+  try {
+    const messages = await Conversation.findOne({
+      participants: participants,
+    }).populate({
+      path: 'messages',
+      options: {
+        limit: pageSize,
+        skip: (pageNumber - 1) * pageSize,
+        sort: { timestamp: -1 },
+      },
+    })
+    res.status(200).json({ succes: true, data: messages })
+  } catch (error) {
+    res.status(400).json({ succes: false, error: error.message })
+  }
+}
+
+module.exports = { getMessages }
