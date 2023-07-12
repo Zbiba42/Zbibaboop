b19f1a04ae28daf869c75a8c890bb8c4ab86d081
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-12 19:40:40 +0100

    getting the sender image then passing it to chats

--- src/components/chat/ChatsContainer.tsx
+++ src/components/chat/ChatsContainer.tsx
@@ -6,12 +6,27 @@ import { NewChat } from './NewChat'
 import { Chat } from './Chat'
 import { profile } from '../Profile/ProfileContent'
 import jwtDecode from 'jwt-decode'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { serverUrl } from '../../config'
 
 export const ChatContainer = () => {
+  const [senderProfile, setSenderProfile] = useState('')
   const newChatShown = useSelector((state: any) => state.Chat.newChatShown)
   const Chats = useSelector((state: any) => state.Chat.Chats)
   const token = sessionStorage.getItem('AccessToken') as string
   const decodedToken = jwtDecode(token) as any
+  const getSenderImg = async () => {
+    const { data } = await axios.get(serverUrl + '/api/user/getUsersImg', {
+      params: {
+        id: decodedToken.id,
+      },
+    })
+    setSenderProfile(data.data.ProfilePath)
+  }
+  useEffect(() => {
+    getSenderImg()
+  }, [])
   return (
     <Box
       sx={{
@@ -27,7 +42,14 @@ export const ChatContainer = () => {
     >
       {newChatShown && <PickMsgReceiver />}
       {Chats.map((chat: { recipient: profile }) => {
-        return <Chat sender={decodedToken.id} recipient={chat.recipient} />
+        return (
+          <Chat
+            senderProfile={senderProfile}
+            sender={decodedToken.id}
+            recipient={chat.recipient}
+            key={chat.recipient._id}
+          />
+        )
       })}
       <NewChat />
     </Box>
