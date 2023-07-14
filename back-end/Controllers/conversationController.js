ffe35cbbb03ff7df4d5bc0c58e4a964354d15109
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-14 17:55:01 +0100

    made the getConvos function return the last message from each conv

--- back-end/Controllers/conversationController.js
+++ back-end/Controllers/conversationController.js
@@ -1,14 +1,23 @@
 const Conversation = require('../models/conversation')
 
 const getConvos = async (req, res) => {
+  const pageSize = 10
+  const pageNumber = req.query.page
   try {
     const id = req.payload.id
     const Conversations = await Conversation.find(
       {
         participants: { $all: [id] },
       },
-      { participants: 1 }
-    )
+      { participants: 1, messages: { $slice: -1 } },
+      {
+        options: {
+          limit: pageSize,
+          skip: (pageNumber - 1) * pageSize,
+          sort: { 'messages.timestamp': -1 },
+        },
+      }
+    ).populate('messages')
     res.status(200).json({ succes: true, data: Conversations })
   } catch (error) {
     res.status(200).json({ succes: true, error: error.message })
