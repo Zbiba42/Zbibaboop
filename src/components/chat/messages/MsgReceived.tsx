8d94462d835b91155ca59967ca1a5392420712f3
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-16 18:07:18 +0100

    code refoactoring made everything looks cleaner and created a component for msg type file

--- src/components/chat/messages/MsgReceived.tsx
+++ src/components/chat/messages/MsgReceived.tsx
@@ -1,40 +1,47 @@
 import { serverUrl } from '../../../config'
+import { FileComponentReceived } from './FileComponentReceived'
 
 interface Props {
   img: string
   content: string
+  files: Array<{ name: string; path: string }>
   isFirst: boolean
 }
 
-export const MsgReceived = ({ img, content, isFirst }: Props) => {
+export const MsgReceived = ({ img, content, files, isFirst }: Props) => {
   return (
     <div className="chat-message mb-1">
       <div className="flex items-end">
-        {isFirst ? (
-          <>
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-              <div>
-                <span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
-                  {content}
-                </span>
-              </div>
-            </div>
-            <img
-              src={serverUrl + img}
-              alt="My profile"
-              className="w-6 h-6 rounded-full order-1 object-cover"
-            />
-          </>
-        ) : (
-          <>
-            <div className="ml-8 flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-              <div>
-                <span className=" px-4 py-2 rounded-lg inline-block rounded-tl-none bg-gray-300 text-gray-600 ">
-                  {content}
-                </span>
-              </div>
-            </div>
-          </>
+        <div
+          className={
+            isFirst
+              ? 'flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start'
+              : 'ml-8 flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start'
+          }
+        >
+          <div>
+            {content && (
+              <span
+                className={
+                  isFirst
+                    ? 'px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600'
+                    : 'px-4 py-2 rounded-lg inline-block rounded-tl-none bg-gray-300 text-gray-600'
+                }
+              >
+                {content}
+              </span>
+            )}
+            {files.length > 0 && (
+              <FileComponentReceived files={files} isFirst={isFirst} />
+            )}
+          </div>
+        </div>
+        {isFirst && (
+          <img
+            src={serverUrl + img}
+            alt="My profile"
+            className="w-6 h-6 rounded-full order-1 object-cover"
+          />
         )}
       </div>
     </div>
--- src/components/chat/messages/MsgSent.tsx
+++ src/components/chat/messages/MsgSent.tsx
@@ -1,38 +1,47 @@
 import { serverUrl } from '../../../config'
+import { FileComponentSent } from './FileComponentSent'
 
 interface Props {
   img: string
   content: string
+  files: Array<{ name: string; path: string }>
   isFirst: boolean
 }
 
-export const MsgSent = ({ img, content, isFirst }: Props) => {
+export const MsgSent = ({ img, content, files, isFirst }: Props) => {
   return (
     <div className="chat-message mb-1">
       <div className="flex items-end justify-end">
-        {isFirst ? (
-          <>
-            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-end">
-              <div>
-                <span className="px-4 py-2 rounded-lg inline-block bg-blue-600 text-white ">
-                  {content}
-                </span>
-              </div>
-            </div>
-            <img
-              src={serverUrl + img}
-              alt="My profile"
-              className="w-6 h-6 rounded-full order-2  object-cover"
-            />
-          </>
-        ) : (
-          <div className="mr-8 flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-            <div>
-              <span className=" px-4 py-2 rounded-lg inline-block rounded-tr-none bg-blue-600 text-white ">
+        <div
+          className={
+            isFirst
+              ? 'flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-end'
+              : 'mr-8 flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end'
+          }
+        >
+          <div>
+            {content && (
+              <span
+                className={
+                  isFirst
+                    ? 'px-4 py-2 rounded-lg inline-block bg-blue-600 text-white'
+                    : 'px-4 py-2 rounded-lg inline-block rounded-tr-none bg-blue-600 text-white'
+                }
+              >
                 {content}
               </span>
-            </div>
+            )}
+            {files.length > 0 && (
+              <FileComponentSent files={files} isFirst={isFirst} />
+            )}
           </div>
+        </div>
+        {isFirst && (
+          <img
+            src={serverUrl + img}
+            alt="My profile"
+            className="w-6 h-6 rounded-full order-2  object-cover"
+          />
         )}
       </div>
     </div>
