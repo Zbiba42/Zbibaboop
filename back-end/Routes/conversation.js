e3ec2465c1566c2bcbde909cc9d93e36a1af3ea5
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-19 16:50:35 +0100

    added route for deleting messages

--- back-end/Routes/conversation.js
+++ back-end/Routes/conversation.js
@@ -3,10 +3,13 @@ const router = require('express').Router()
 const {
   getConvos,
   getMessages,
+  removeMessage,
 } = require('../Controllers/conversationController')
 
 router.get('/convos', getConvos)
 
 router.get('/messages', getMessages)
+
+router.post('/messages/remove', removeMessage)
 
 module.exports = router
