a216bebcc534dfdb86a69be69380d4b9520389c1
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-21 18:42:44 +0100

    removed the delete function for the received messages

--- src/components/chat/messages/MsgReceived.tsx
+++ src/components/chat/messages/MsgReceived.tsx
@@ -1,8 +1,6 @@
-import { IconButton, Tooltip, Menu, MenuItem } from '@mui/material'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { Tooltip } from '@mui/material'
 import { serverUrl } from '../../../config'
 import { FileComponentReceived } from './FileComponentReceived'
-import { useState } from 'react'
 
 interface Props {
   img: string
@@ -17,18 +15,9 @@ interface Props {
     _id: string
   }
   isFirst: boolean
-  deleteMessage: (id: any) => Promise<void>
 }
 
-export const MsgReceived = ({ img, msg, isFirst, deleteMessage }: Props) => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
-  const open = Boolean(anchorEl)
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
+export const MsgReceived = ({ img, msg, isFirst }: Props) => {
   return (
     <div className="chat-message mb-1">
       <div className="flex items-end">
@@ -65,39 +54,8 @@ export const MsgReceived = ({ img, msg, isFirst, deleteMessage }: Props) => {
               {msg.files.length > 0 && (
                 <FileComponentReceived files={msg.files} isFirst={isFirst} />
               )}
-              <IconButton onClick={handleClick}>
-                <MoreVertIcon fontSize="small" />
-              </IconButton>
             </div>
           </Tooltip>
-
-          <Menu
-            id="demo-positioned-menu"
-            aria-labelledby="demo-positioned-button"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-          >
-            <MenuItem onClick={handleClose}>Edit</MenuItem>
-            <MenuItem
-              onClick={() => {
-                handleClose()
-                setTimeout(() => {
-                  deleteMessage(msg._id)
-                }, 10)
-              }}
-            >
-              Delete
-            </MenuItem>
-          </Menu>
         </div>
         {isFirst && (
           <img
