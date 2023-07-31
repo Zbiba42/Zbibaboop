397401e2a50a157e9bf6b95b51943996f2d7c315
Zbiba <abdrrahmanabdo46@gmail.com>
2023-07-31 16:59:47 +0100

    made the tags in the post model an array of users

--- back-end/models/post.js
+++ back-end/models/post.js
@@ -5,11 +5,13 @@ const PostSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     required: true,
   },
-  tags: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: false,
-  },
+  tags: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: false,
+    },
+  ],
   content: {
     type: String,
     required: true,
