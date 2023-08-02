4772d90c96bcd99378ed8a48a72e22a103848cd3
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-02 18:55:32 +0100

    added routes for the new functions

--- back-end/Routes/post.js
+++ back-end/Routes/post.js
@@ -1,11 +1,21 @@
 const { uploadPostFiles } = require('../middlewares/multerImages')
-const { addPost, getPosts } = require('../Controllers/postController')
+const {
+  addPost,
+  getPosts,
+  UpdatePostContent,
+  UpdatePostTags,
+  DeletePost,
+} = require('../Controllers/postController')
 
 const router = require('express').Router()
 
 const postRouter = (io) => {
   router.post('/create', uploadPostFiles, (req, res) => addPost(io, req, res))
   router.get('/get', getPosts)
+  router.post('/updateContent', UpdatePostContent)
+  router.post('/updateTags', UpdatePostTags)
+  router.post('/delete', DeletePost)
+
   return router
 }
 
