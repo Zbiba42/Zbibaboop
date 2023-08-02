bd7fdc8e691c458acadfaf8faddfc332faefcab7
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-02 18:53:36 +0100

    added a ref for the user collection

--- back-end/models/post.js
+++ back-end/models/post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const PostSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
     required: true,
   },
   tags: [
