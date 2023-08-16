985a0c5fea1ec1f16cf3814515a0e91bcc1c9326
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-16 18:33:10 +0100

    added route for getting single post / getting post comments

--- back-end/Routes/post.js
+++ back-end/Routes/post.js
@@ -2,9 +2,11 @@ const { uploadPostFiles } = require('../middlewares/multerImages')
 const {
   addPost,
   getPosts,
+  getPost,
   UpdatePostContent,
   UpdatePostTags,
   DeletePost,
+  getPostComments,
 } = require('../Controllers/postController')
 
 const router = require('express').Router()
@@ -12,10 +14,11 @@ const router = require('express').Router()
 const postRouter = (io) => {
   router.post('/create', uploadPostFiles, (req, res) => addPost(io, req, res))
   router.get('/get', getPosts)
+  router.get('/getOne', getPost)
   router.post('/updateContent', UpdatePostContent)
   router.post('/updateTags', UpdatePostTags)
   router.post('/delete', DeletePost)
-
+  router.get('/getComments', getPostComments)
   return router
 }
 
