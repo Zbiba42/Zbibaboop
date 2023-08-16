bf7e41c419032b180f31cb26a813f10896041c04
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-16 18:40:38 +0100

    style changes for the search input

--- src/components/search/SearchInput.tsx
+++ src/components/search/SearchInput.tsx
@@ -4,20 +4,27 @@ import { useNavigate } from 'react-router-dom'
 export const SearchInput = () => {
   const navigate = useNavigate()
   return (
-    <TextField
-      placeholder="Search"
-      variant="outlined"
-      sx={{ width: '40%', position: 'fixed', left: '25rem', top: '1.8rem' }}
-      onKeyDown={(e: any) => {
-        e.key == 'Enter' && e.target.value.trim() != ''
-          ? navigate(
-              `/search?filter=all&search=${encodeURIComponent(
-                e.target.value.trim()
-              )}`
-            )
-          : ''
-      }}
-      autoComplete="off"
-    />
+    <div className="w-full ml-[84px] h-[89px] fixed bg-white pt-[1.8rem] z-50">
+      <TextField
+        placeholder="Search"
+        variant="outlined"
+        sx={{
+          width: '40%',
+          position: 'fixed',
+          left: '25rem',
+          backgroundColor: 'white',
+        }}
+        onKeyDown={(e: any) => {
+          e.key == 'Enter' && e.target.value.trim() != ''
+            ? navigate(
+                `/search?filter=all&search=${encodeURIComponent(
+                  e.target.value.trim()
+                )}`
+              )
+            : ''
+        }}
+        autoComplete="off"
+      />
+    </div>
   )
 }
