2c807e0a3c3579392545d077ad6a649fc6fc804e
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-16 18:29:57 +0100

    updated the comments model

--- back-end/models/comment.js
+++ back-end/models/comment.js
@@ -3,48 +3,22 @@ const mongoose = require('mongoose')
 const CommentSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
+    ref: 'User',
     required: true,
   },
   onPost: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Post',
-    required: false,
-  },
-  isReply: {
-    type: Boolean,
-    default: false,
-  },
-  onComment: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Comment',
-    required: false,
+    required: true,
   },
   content: {
     type: String,
     required: true,
   },
-  files: {
-    type: Array,
-    required: false,
-  },
   timestamp: {
     type: Date,
     default: Date.now,
   },
-  reactions: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Reaction',
-      required: false,
-    },
-  ],
-  replies: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Comment',
-    },
-  ],
 })
 
 module.exports = mongoose.model('Comment', CommentSchema)
