97f96974c8138d4c2cb4e176c1a533095031795e
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-16 18:38:22 +0100

    finished the post component

--- src/components/Profile/tabs/posts/post/Post.tsx
+++ src/components/Profile/tabs/posts/post/Post.tsx
@@ -5,8 +5,17 @@ import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
-import { Button, ListItemIcon, TextField } from '@mui/material'
-import { useRef, useState } from 'react'
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  ListItemIcon,
+  TextField,
+} from '@mui/material'
+import { useEffect, useRef, useState } from 'react'
 import { PostFilesPrev } from './PostFilesPrev'
 import { toast } from 'react-toastify'
 import Box from '@mui/material/Box'
@@ -17,13 +26,18 @@ import ShareIcon from '@mui/icons-material/Share'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import SentimentSatisfiedIcon from '@mui/icons-material/SentimentSatisfied'
 import axios from 'axios'
+import { Comments } from './Comments'
+import { Link } from 'react-router-dom'
+import jwtDecode from 'jwt-decode'
 
 interface Props {
   post: PostInterface
   user?: profile
+  setRefresh: React.Dispatch<React.SetStateAction<number>>
 }
 
-export const Post = ({ post, user }: Props) => {
+export const Post = ({ post, user, setRefresh }: Props) => {
+  const [isSamePerson, setIsSamePerson] = useState(false)
   const [isExpanded, setIsExpanded] = useState(false)
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
   const open = Boolean(anchorEl)
@@ -38,9 +52,18 @@ export const Post = ({ post, user }: Props) => {
   const handleReactHover = (hover: boolean) => {
     setIsReactHovered(hover)
   }
+  const [DialogOpen, setDialogOpen] = useState(false)
 
+  const handleDialogClickOpen = () => {
+    setDialogOpen(true)
+  }
+
+  const handleDialogClose = () => {
+    setDialogOpen(false)
+  }
   const [isEdittingContent, SetEdittingContent] = useState(false)
   const PostContentRef = useRef<HTMLInputElement>(null)
+  const [isCommentsShown, setCommentsShown] = useState(false)
   const updateContent = async () => {
     if (PostContentRef.current) {
       if (PostContentRef.current.value !== post.content) {
@@ -63,8 +86,38 @@ export const Post = ({ post, user }: Props) => {
       }
     }
   }
+  const deletePost = async () => {
+    try {
+      const { data } = await axios.post(serverUrl + '/api/posts/delete', {
+        postId: post._id,
+      })
+      if (data.succes) {
+        toast.success('post deleted successfully')
+        setRefresh((old) => old + 1)
+      }
+    } catch (error: any) {
+      toast.error(error.message)
+    }
+  }
+  useEffect(() => {
+    const accessToken = sessionStorage.getItem('AccessToken')
+    const decodedToken = accessToken
+      ? jwtDecode<{ id: string }>(accessToken)
+      : null
+
+    if (decodedToken) {
+      if (decodedToken.id === post.owner._id) {
+        setIsSamePerson(true)
+      } else {
+        setIsSamePerson(false)
+      }
+    }
+  }, [])
   return (
-    <div className="w-full border min-h-[15rem]  p-2 my-2 rounded-md relative">
+    <div
+      className="w-full border min-h-[auto]  p-2 my-2 rounded-md relative"
+      onMouseLeave={() => handleReactHover(false)}
+    >
       <Menu
         id="long-menu"
         MenuListProps={{
@@ -86,57 +139,105 @@ export const Post = ({ post, user }: Props) => {
           </ListItemIcon>
           edit post tags
         </MenuItem>
-        <MenuItem className="hover:bg-[#D52941]">
+        <MenuItem
+          className="hover:bg-[#D52941]"
+          onClick={handleDialogClickOpen}
+        >
           <ListItemIcon>
             <i className="fa-solid fa-trash"></i>
           </ListItemIcon>
           delete post
         </MenuItem>
       </Menu>
-
+      {/* delete dialog */}
+      <Dialog
+        open={DialogOpen}
+        onClose={handleDialogClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Delete Post'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure u want to delete the post ? once the post is deleted
+            you cant restore it!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button
+            onClick={() => {
+              deletePost()
+              handleDialogClose()
+            }}
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
       {/* Nav */}
 
       <div className="flex items-center gap-3 relative">
         <div className="absolute top-1 right-0">
-          <IconButton
-            aria-label="more"
-            id="long-button"
-            aria-controls={open ? 'long-menu' : undefined}
-            aria-expanded={open ? 'true' : undefined}
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            <MoreVertIcon />
-          </IconButton>
+          {isSamePerson && (
+            <IconButton
+              aria-label="more"
+              id="long-button"
+              aria-controls={open ? 'long-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+          )}
         </div>
-        <img
-          src={serverUrl + user?.ProfilePath}
-          alt=""
-          draggable="false"
-          className=" w-12 h-12 object-cover rounded-full border border-black "
-        />
+        <Link to={'/user/' + user?._id}>
+          <img
+            src={serverUrl + user?.ProfilePath}
+            alt=""
+            draggable="false"
+            className=" w-12 h-12 object-cover rounded-full border border-black "
+          />
+        </Link>
         <div>
-          <h1 className=" font-bold text-lg text-[#272838] capitalize text-left ">
-            {user?.Fullname}
-          </h1>
-          <h1 className=" font-sans text-sm text-[#272838] capitalize text-left ">
-            with
-            {post.tags.splice(0, 2).map((tag: any) => {
-              return (
-                <>
-                  <span
-                    className="text-[#272838] space-y-2 border- rounded-xl bg-blue-200 px-2 mx-1 mb-1 cursor-pointer transition-all duration-200"
-                    key={tag._id}
-                  >
-                    {tag.Fullname}
-                    {post.tags.length - 3 > 0 && (
-                      <span>And {post.tags.length - 3}</span>
-                    )}
-                  </span>
-                </>
-              )
-            })}
-          </h1>
+          <Link to={'/user/' + user?._id}>
+            <h1 className=" font-bold text-lg text-[#272838] capitalize text-left ">
+              {user?.Fullname}
+            </h1>
+          </Link>
+          <Link to={'/post/' + post._id}>
+            <h4 className="font-sans text-sm text-[#272838] capitalize text-left">
+              {new Date(post.timestamp).toLocaleString('en-US', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+                hour: 'numeric',
+                minute: 'numeric',
+              })}
+            </h4>
+          </Link>
+          {post.tags.length > 0 && (
+            <h1 className=" font-sans text-sm text-[#272838] capitalize text-left ">
+              with
+              {post.tags.splice(0, 2).map((tag: any) => {
+                return (
+                  <>
+                    <span
+                      className="text-[#272838] space-y-2 border- rounded-xl bg-blue-200 px-2 mx-1 mb-1 cursor-pointer transition-all duration-200"
+                      key={tag._id}
+                    >
+                      {tag.Fullname}
+                      {post.tags.length - 3 > 0 && (
+                        <span>And {post.tags.length - 3}</span>
+                      )}
+                    </span>
+                  </>
+                )
+              })}
+            </h1>
+          )}
         </div>
       </div>
       {/* Content */}
@@ -208,7 +309,6 @@ export const Post = ({ post, user }: Props) => {
         <div
           className="flex flex-row justify-center items-center w-full space-x-3"
           onMouseEnter={() => handleReactHover(true)}
-          onMouseLeave={() => handleReactHover(false)}
         >
           <ThumbUpIcon
             sx={{ color: isReactHovered ? 'primary.main' : '#838383' }}
@@ -219,8 +319,7 @@ export const Post = ({ post, user }: Props) => {
               sx={{
                 position: 'absolute',
                 top: '-4rem',
-                left: '50%',
-                transform: 'translateX(-50%)',
+                left: '0',
                 backgroundColor: 'white',
                 borderRadius: '10px',
                 boxShadow: '0 1px 6px rgba(0, 0, 0, 0.1)',
@@ -230,8 +329,9 @@ export const Post = ({ post, user }: Props) => {
             >
               <Stack
                 direction="row"
-                spacing={1}
+                spacing={2}
                 onMouseEnter={() => handleReactHover(true)}
+                onMouseLeave={() => handleReactHover(false)}
               >
                 <IconButton
                   size="large"
@@ -246,12 +346,6 @@ export const Post = ({ post, user }: Props) => {
                 <IconButton size="large" color="primary" aria-label={'Love'}>
                   <SentimentSatisfiedIcon />
                 </IconButton>
-                <IconButton size="large" color="primary" aria-label={'Comment'}>
-                  <ChatBubbleOutlineIcon />
-                </IconButton>
-                <IconButton size="large" color="primary" aria-label={'Share'}>
-                  <ShareIcon />
-                </IconButton>
               </Stack>
             </Box>
           )}
@@ -263,6 +357,7 @@ export const Post = ({ post, user }: Props) => {
             backgroundColor: 'transparent',
             '&:hover': { backgroundColor: 'transparent' },
           }}
+          onClick={() => setCommentsShown((old) => !old)}
         >
           <ChatBubbleOutlineIcon />
         </IconButton>
@@ -276,6 +371,8 @@ export const Post = ({ post, user }: Props) => {
           <ShareIcon />
         </IconButton>
       </div>
+      {/* Comments */}
+      {isCommentsShown && <Comments post={post} />}
     </div>
   )
 }
