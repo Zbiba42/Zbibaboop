5ec73a5e5e130eef4cf037720c8e778b3fa102ef
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-17 18:51:00 +0100

    finished the search backend

--- back-end/Controllers/searchController.js
+++ back-end/Controllers/searchController.js
@@ -1,37 +1,48 @@
 const User = require('../models/user')
+const Post = require('../models/post')
 
 const search = async (req, res) => {
   try {
     const query = req.query.search
-    const page = parseInt(req.query.page) || 1 // Current page number (default: 1)
-    const limit = parseInt(req.query.limit) || 10 // Results per page (default: 10)
-    const skip = (page - 1) * limit // Number of results to skip
-    const users = await User.find(
-      {
-        Fullname: { $regex: query, $options: 'i' },
-      },
-      {
-        Email: 0,
-        Verified: 0,
-        Password: 0,
-        friendRequestsReceived: 0,
-        friendRequestsSent: 0,
-        notifications: 0,
-      }
-    )
-      .skip(skip)
-      .limit(limit)
-    const totalUsers = await User.countDocuments({
-      Fullname: { $regex: query, $options: 'i' },
-    })
-
-    const totalPages = Math.ceil(totalUsers / limit)
-    res.status(200).json({
-      success: true,
-      data: users,
-      currentPage: page,
-      totalPages: totalPages,
-    })
+    const filter = req.query.filter
+    const pageNumber = req.query.page
+    const pageSize = 5
+    if (filter == 'People') {
+      const users = await User.find(
+        {
+          Fullname: { $regex: query, $options: 'i' },
+        },
+        {
+          Email: 0,
+          Verified: 0,
+          Password: 0,
+          friendRequestsReceived: 0,
+          friendRequestsSent: 0,
+          notifications: 0,
+        }
+      )
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+      res.status(200).json({
+        success: true,
+        data: users,
+      })
+    } else if (filter == 'Posts') {
+      const posts = await Post.find({
+        content: { $regex: query, $options: 'i' },
+      })
+        .populate({
+          path: 'tags owner',
+        })
+        .skip((pageNumber - 1) * pageSize)
+        .limit(pageSize)
+      res.status(200).json({
+        success: true,
+        data: posts,
+      })
+    } else {
+      res.status(400).json({ succes: false, data: [] })
+    }
   } catch (error) {
     res.status(400).json({ succes: false, error: error.message })
   }
