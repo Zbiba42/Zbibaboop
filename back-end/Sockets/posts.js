cf207d5b99b92fe88ed1368c7388fbb133098c9d
Zbiba <abdrrahmanabdo46@gmail.com>
2023-08-17 18:52:22 +0100

    commented unused code and fixed a notification error

--- back-end/Sockets/posts.js
+++ back-end/Sockets/posts.js
@@ -65,10 +65,10 @@ const Posts = (io, socket) => {
     }
     try {
       const comment = await Comment.create(commentObj)
-      const post = await Post.findOne({ _id: data.postId })
+      const post = await Post.findOne({ _id: data.postId }).populate('owner')
       post.comments.push(comment)
       await post.save()
-      if (data.id !== post.owner) {
+      if (data.id != post.owner._id) {
         const notification = await Notification.create({
           sender: data.id,
           receiver: post.owner,
@@ -89,128 +89,130 @@ const Posts = (io, socket) => {
       io.to(data.id).emit('commentResponse', { succes: false })
     }
   })
+
+  //// Unused code (may add these features later)
   //////////////////////////////////////////
-  socket.on('commentReply', async (data) => {
-    let replyObj = {
-      owner: data.id,
-      onComment: data.commentId,
-      content: data.content,
-      isReply: true,
-      files: [],
-    }
+  // socket.on('commentReply', async (data) => {
+  //   let replyObj = {
+  //     owner: data.id,
+  //     onComment: data.commentId,
+  //     content: data.content,
+  //     isReply: true,
+  //     files: [],
+  //   }
 
-    if (data.files) {
-      const filePath = `./uploads/${data.id}`
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
+  //   if (data.files) {
+  //     const filePath = `./uploads/${data.id}`
+  //     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9)
 
-      for (const file of data.files) {
-        const extension = file.fileName.split('.').pop()
-        const fileName = `file-${uniqueSuffix}.${extension}`
-        const fileDestination = path.join(filePath, fileName)
+  //     for (const file of data.files) {
+  //       const extension = file.fileName.split('.').pop()
+  //       const fileName = `file-${uniqueSuffix}.${extension}`
+  //       const fileDestination = path.join(filePath, fileName)
 
-        try {
-          const fileData = Buffer.from(file.file)
-          if (!fs.existsSync(filePath)) {
-            fs.mkdirSync(filePath, { recursive: true })
-          }
-          fs.writeFile(
-            fileDestination,
-            fileData,
-            {
-              encoding: 'binary',
-            },
-            (err) => {
-              if (err) {
-                io.to(data.id).emit('commentResponse', {
-                  succes: false,
-                })
-                return
-              }
-            }
-          )
-          replyObj.files.push(fileDestination)
-        } catch (error) {
-          io.to(data.id).emit('commentResponse', { succes: false })
-        }
-      }
-    }
-    try {
-      const reply = await Comment.create(replyObj)
-      const comment = await Comment.findOne({ _id: data.commentId })
-      comment.replies.push(reply)
-      await comment.save()
+  //       try {
+  //         const fileData = Buffer.from(file.file)
+  //         if (!fs.existsSync(filePath)) {
+  //           fs.mkdirSync(filePath, { recursive: true })
+  //         }
+  //         fs.writeFile(
+  //           fileDestination,
+  //           fileData,
+  //           {
+  //             encoding: 'binary',
+  //           },
+  //           (err) => {
+  //             if (err) {
+  //               io.to(data.id).emit('commentResponse', {
+  //                 succes: false,
+  //               })
+  //               return
+  //             }
+  //           }
+  //         )
+  //         replyObj.files.push(fileDestination)
+  //       } catch (error) {
+  //         io.to(data.id).emit('commentResponse', { succes: false })
+  //       }
+  //     }
+  //   }
+  //   try {
+  //     const reply = await Comment.create(replyObj)
+  //     const comment = await Comment.findOne({ _id: data.commentId })
+  //     comment.replies.push(reply)
+  //     await comment.save()
 
-      const notification = await Notification.create({
-        sender: data.id,
-        receiver: comment.owner,
-        type: 'commentReply',
-        content: { commentId: comment._id },
-      })
-      await User.updateOne(
-        { _id: comment.owner },
-        {
-          $push: {
-            notifications: notification,
-          },
-        }
-      )
-      io.to(comment.owner).emit('notification')
-    } catch (error) {
-      io.to(data.id).emit('commentResponse', { succes: false })
-    }
-  })
-  //////////////////////////////////////////
-  socket.on('commentReaction', async (data) => {
-    try {
-      let reaction = await Reaction.findOne({
-        owner: data.id,
-        onComment: data.commentId,
-      })
-      const comment = await Comment.findOne({ _id: data.commentId })
-      if (!reaction) {
-        reaction = await Reaction.create({
-          owner: data.id,
-          onComment: data.postId,
-          reaction: data.reactionType,
-        })
-        await Comment.updateOne(
-          { _id: data.commentId },
-          { $addToSet: { reactions: reaction } }
-        )
-        const notification = await Notification.create({
-          sender: data.id,
-          receiver: comment.owner,
-          type: 'commentReaction',
-          content: { postId: comment._id },
-        })
-        await User.updateOne(
-          { _id: comment.owner },
-          {
-            $push: {
-              notifications: notification,
-            },
-          }
-        )
-        io.to(comment.owner).emit('notification')
-      }
-      if (reaction.reaction !== data.reactionType) {
-        reaction.reaction = data.reactionType
-        await reaction.save()
+  //     const notification = await Notification.create({
+  //       sender: data.id,
+  //       receiver: comment.owner,
+  //       type: 'commentReply',
+  //       content: { commentId: comment._id },
+  //     })
+  //     await User.updateOne(
+  //       { _id: comment.owner },
+  //       {
+  //         $push: {
+  //           notifications: notification,
+  //         },
+  //       }
+  //     )
+  //     io.to(comment.owner).emit('notification')
+  //   } catch (error) {
+  //     io.to(data.id).emit('commentResponse', { succes: false })
+  //   }
+  // })
+  // //////////////////////////////////////////
+  // socket.on('commentReaction', async (data) => {
+  //   try {
+  //     let reaction = await Reaction.findOne({
+  //       owner: data.id,
+  //       onComment: data.commentId,
+  //     })
+  //     const comment = await Comment.findOne({ _id: data.commentId })
+  //     if (!reaction) {
+  //       reaction = await Reaction.create({
+  //         owner: data.id,
+  //         onComment: data.postId,
+  //         reaction: data.reactionType,
+  //       })
+  //       await Comment.updateOne(
+  //         { _id: data.commentId },
+  //         { $addToSet: { reactions: reaction } }
+  //       )
+  //       const notification = await Notification.create({
+  //         sender: data.id,
+  //         receiver: comment.owner,
+  //         type: 'commentReaction',
+  //         content: { postId: comment._id },
+  //       })
+  //       await User.updateOne(
+  //         { _id: comment.owner },
+  //         {
+  //           $push: {
+  //             notifications: notification,
+  //           },
+  //         }
+  //       )
+  //       io.to(comment.owner).emit('notification')
+  //     }
+  //     if (reaction.reaction !== data.reactionType) {
+  //       reaction.reaction = data.reactionType
+  //       await reaction.save()
 
-        await Comment.updateOne(
-          { _id: data.commentId },
-          { $pull: { reactions: { owner: data.id } } }
-        )
+  //       await Comment.updateOne(
+  //         { _id: data.commentId },
+  //         { $pull: { reactions: { owner: data.id } } }
+  //       )
 
-        await Comment.updateOne(
-          { _id: data.commentId },
-          { $addToSet: { reactions: reaction } }
-        )
-      }
-    } catch (error) {
-      io.to(data.id).emit('reactionResponse', { succes: false })
-    }
-  })
+  //       await Comment.updateOne(
+  //         { _id: data.commentId },
+  //         { $addToSet: { reactions: reaction } }
+  //       )
+  //     }
+  //   } catch (error) {
+  //     io.to(data.id).emit('reactionResponse', { succes: false })
+  //   }
+  // })
 }
 
 module.exports = { Posts }
